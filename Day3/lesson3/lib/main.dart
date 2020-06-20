5e0671c66e0af3cc00d3741b267be45698ff6f69
mnhazim <hazim.sap1@gmail.com>
2020-06-20 13:07:57 +0800

    demo shopping cart provider

---
+++ Day3/lesson3/lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [
+        Provider(create: (context) => CatalogModel()),
+        ChangeNotifierProxyProvider<CatalogModel, CartModel>(
+          create: (context) => CartModel(),
+          update: (context, catalog, cart){
+            cart.catalog = catalog;
+            return cart;
+          } ,
+        )
+      ],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Provider Demo',
+        theme: appTheme,
+        initialRoute: '/',
+      ),
+    );
+  }
+}
