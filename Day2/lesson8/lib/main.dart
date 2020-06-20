2c1c10a7a4288af875d3d61ee4834403f02819f4
mnhazim <hazim.sap1@gmail.com>
2020-06-20 12:12:49 +0800

    modified:   lib/main.dart 	modified:   pubspec.lock 	modified:   pubspec.yaml 	modified:   ../../assignment1

--- Day2/lesson8/lib/main.dart
+++ Day2/lesson8/lib/main.dart
@@ -1,7 +1,9 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:lesson8/listpage.dart';
 
+final FirebaseAuth _auth = FirebaseAuth.instance;
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
@@ -13,46 +15,118 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: Text('Material App Bar'),
         ),
-        body: Home(),
+        body: Builder(builder: (BuildContext context) {
+          return ListView(
+            scrollDirection: Axis.vertical,
+            children: <Widget>[
+              _EmailPasswordForm(),
+            ],
+          );
+        }),
       ),
-      routes: <String, WidgetBuilder> {
-        'list' : (BuildContext context) => Listpage()
+      routes: <String, WidgetBuilder>{
+        'list': (BuildContext context) => Listpage()
       },
     );
   }
 }
 
-class Home extends StatelessWidget {
-  final _namaController = TextEditingController();
-  final _emailController = TextEditingController();
+class _EmailPasswordForm extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => _EmailPasswordFormState();
+}
 
+class _EmailPasswordFormState extends State<_EmailPasswordForm> {
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+  bool _success;
+  String _userEmail;
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: ListView(
+    return Form(
+      key: _formKey,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: <Widget>[
-          TextField(controller: _namaController),
-          TextField(controller: _emailController),
-          RaisedButton(
-            child: Text("Save Data"),
-            onPressed: () async {
-              await Firestore.instance
-                  .collection('pengguna')
-                  .document()
-                  .setData({
-                'nama': _namaController.text,
-                'noic': _emailController.text,
-              });
+          Container(
+            child: const Text('Test sign in with email and password'),
+            padding: const EdgeInsets.all(16),
+            alignment: Alignment.center,
+          ),
+          TextFormField(
+            controller: _emailController,
+            decoration: const InputDecoration(labelText: 'Email'),
+            validator: (String value) {
+              if (value.isEmpty) {
+                return 'Please enter some text';
+              }
+              return null;
             },
           ),
-          RaisedButton(
-            child: Text("List"),
-            onPressed: () {
-              Navigator.pushNamed(context, 'list');
+          TextFormField(
+            controller: _passwordController,
+            decoration: const InputDecoration(labelText: 'Password'),
+            validator: (String value) {
+              if (value.isEmpty) {
+                return 'Please enter some text';
+              }
+              return null;
             },
           ),
+          Container(
+            padding: const EdgeInsets.symmetric(vertical: 16.0),
+            alignment: Alignment.center,
+            child: RaisedButton(
+              onPressed: () async {
+                if (_formKey.currentState.validate()) {
+                  _signInWithEmailAndPassword();
+                }
+              },
+              child: const Text('Submit'),
+            ),
+          ),
+          Container(
+            alignment: Alignment.center,
+            padding: const EdgeInsets.symmetric(horizontal: 16),
+            child: Text(
+              _success == null
+                  ? ''
+                  : (_success
+                      ? 'Successfully signed in ' + _userEmail
+                      : 'Sign in failed'),
+              style: TextStyle(color: Colors.red),
+            ),
+          )
         ],
       ),
     );
+  }
+
+  @override
+  void dispose() {
+    _emailController.dispose();
+    _passwordController.dispose();
+    super.dispose();
+  }
+
+  // Example code of how to sign in with email and password.
+  void _signInWithEmailAndPassword() async {
+    final FirebaseUser user = (await _auth.signInWithEmailAndPassword(
+      email: _emailController.text,
+      password: _passwordController.text,
+    ))
+        .user;
+    if (user != null) {
+      setState(() {
+        _success = true;
+        _userEmail = user.email;
+        print(user.uid);
+        print(user.photoUrl);
+      });
+    } else {
+      _success = false;
+      print('invalid');
+    }
   }
 }
